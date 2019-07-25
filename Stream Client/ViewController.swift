7afaa272329bf2bd9c6a40cf2a0184c7e413f790
Carlos Bedoy <carlos.bedoy@gmail.com>
2019-07-25 10:26:48 -0500

    Initial Commit

---
+++ Stream Client/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Stream Client
+//
+//  Created by Carlos Bedoy on 7/25/19.
+//  Copyright © 2019 iambedoy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Stream ClientTests/Stream_ClientTests.swift
@@ -0,0 +1,34 @@
+//
+//  Stream_ClientTests.swift
+//  Stream ClientTests
+//
+//  Created by Carlos Bedoy on 7/25/19.
+//  Copyright © 2019 iambedoy. All rights reserved.
+//
+
+import XCTest
+@testable import Stream_Client
+
+class Stream_ClientTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
